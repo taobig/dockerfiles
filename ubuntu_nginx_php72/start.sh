db6f67a5e8d2f8554247ffbbdb74d424f00988bd
taobig <studentht@hotmail.com>
2023-02-05 18:42:01 +0800

    Move PHP72 Dockerfile to eol branch

--- ubuntu_nginx_php72/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/local/php/sbin/php-fpm
-#/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
-/usr/local/nginx/sbin/nginx
-
-#tail -f /start.sh
--- ubuntu_nginx_php72_phalcon/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/local/php/sbin/php-fpm
-/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
-
-tail -f /start.sh
