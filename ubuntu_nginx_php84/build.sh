4a2dc611bea7c8169b0488769f10d5382dc00e45
taobig <studentht@hotmail.com>
2026-02-11 23:24:45 +0800

    使用docker buildx支持多平台构建

--- ubuntu_nginx_php84/build.sh
+++ ubuntu_nginx_php84/build.sh
@@ -5,4 +5,7 @@ docker login
 #docker build --pull -t taobig/nginx-php84 . || exit 1;
 #docker push taobig/nginx-php84 || exit 1;
 
-docker build --pull -t taobig/nginx-php84 --push .
+#docker build --pull -t taobig/nginx-php84 --push .
+#docker buildx build --pull --platform linux/amd64,linux/arm64 -t taobig/nginx-php84 --push .
+# TODO: 对于arm64平台，Dockerfile中链接库路径需要修改。
+docker buildx build --pull --platform linux/amd64 -t taobig/nginx-php84 --push .
