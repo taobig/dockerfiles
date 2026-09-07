23b915e93a830ebc68834dbdfad427611374d6a6
taobig <studentht@hotmail.com>
2026-09-07 23:56:11 +0800

    让 nginx-php84 运行时库按 multiarch 目录导出，以便 arm64 与 amd64 共用同一份 Dockerfile。

--- ubuntu_nginx_php84/build.sh
+++ ubuntu_nginx_php84/build.sh
@@ -6,6 +6,4 @@ docker login
 #docker push taobig/nginx-php84 || exit 1;
 
 #docker build --pull -t taobig/nginx-php84 --push .
-#docker buildx build --pull --platform linux/amd64,linux/arm64 -t taobig/nginx-php84 --push .
-# TODO: 对于arm64平台，Dockerfile中链接库路径需要修改。
-docker buildx build --pull --platform linux/amd64 -t taobig/nginx-php84 --push .
+docker buildx build --pull --platform linux/amd64,linux/arm64 -t taobig/nginx-php84 --push .
